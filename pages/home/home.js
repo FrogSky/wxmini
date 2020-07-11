e3135cd06ac88313744d43cc508d44f5c50e13f4
张宁 <zhangning@qding.me>
2020-07-11 16:59:46 +0800

    wx

---
+++ pages/home/home.js
@@ -0,0 +1,89 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgList:[{
+      url: '../../images/google.jpg',
+      posName: '跳跳床',
+      desc: '跳啊跳跳啊跳跳啊跳跳啊跳',
+      money: '2m-90m',
+      opendate: '2020-01-01'
+    },{
+      url: '../../images/google.jpg',
+      posName: '跳跳床',
+      desc: '跳啊跳跳啊跳跳啊跳跳啊跳',
+      money: '2m-90m',
+      opendate: '2020-01-01'
+    },{
+      url: '../../images/google.jpg',
+      posName: '跳跳床',
+      desc: '跳啊跳跳啊跳跳啊跳跳啊跳',
+      money: '2m-90m',
+      opendate: '2020-01-01'
+    }]
+  },
+  tabshow(){
+    console.log(111)
+    wx.navigateTo({
+      url: '../../pages/jobdetail/jobdetail'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
